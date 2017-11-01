f4ceb1215436bf2594075b29beb4f28df5331d71
Shawn Shrader <hawkbit@gmail.com>
2017-11-01 16:19:33 -0700

    org c1

--- 1_scanner_complete.py
+++ 1_scanner_complete.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import socket
+import os
+import sys
+
 
 def retBanner(ip, port):
     try:
-        socket.setdefaulttimeout(3)
+        socket.setdefaulttimeout(2)
         s = socket.socket()
         s.connect((ip, port))
         banner = s.recv(1024)
@@ -10,30 +15,43 @@ def retBanner(ip, port):
     except:
         return
 
-def checkVulns(banner):
-    if 'FreeFloat Ftp Server (Version 1.00)' in banner:
-        print '[+] FreeFloat FTP Server is vulnerable.'
-    elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
-        print '[+] 3CDaemon FTP Server is vulnerable.'
-    elif 'Ability Server 2.34' in banner:
-        print '[+] Ability FTP Server is vulnerable.'
-    elif 'Sami FTP Server 2.0.2' in banner:
-        print '[+] Sami FTP Server is vulnerable.'
-    elif "220 Welcome to Adobe FTP services" in banner:
-        print "[+] Adobe FTP Server is vulnerable."
-    else:
-        print '[-] FTP Server is not vulnerable.'
-    return
+
+def checkVulns(banner, filename):
+
+    f = open(filename, 'r')
+    for line in f.readlines():
+        if line.strip('\n') in banner:
+            print '[+] Server is vulnerable: ' +\
+                banner.strip('\n')
+
 
 def main():
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print '[-] ' + filename +\
+                ' does not exist.'
+            exit(0)
+
+        if not os.access(filename, os.R_OK):
+            print '[-] ' + filename +\
+                ' access denied.'
+            exit(0)
+    else:
+        print '[-] Usage: ' + str(sys.argv[0]) +\
+            ' <vuln filename>'
+        exit(0)
+
     portList = [21,22,25,80,110,443]
-    for x in range(1, 255):
+    for x in range(147, 150):
         ip = '192.168.95.' + str(x)
         for port in portList:
-        #    print ip + ":" + str(port)
             banner = retBanner(ip, port)
-        #    if banner:
-        #        print '[+] ' + ip + ': ' + banner
-                #checkVulns(banner)
+            if banner:
+                print '[+] ' + ip + ' : ' + banner
+                checkVulns(banner, filename)
+
+
 if __name__ == '__main__':
     main()
