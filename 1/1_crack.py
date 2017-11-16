cc6538769ef3e5343d49a39730aaab0b3c064f5e
Shawn Shrader <hawkbit@gmail.com>
2017-11-16 09:50:00 -0800

    added 1 crack.py for linux

--- 1/1_crack.py
+++ 1/1_crack.py
@@ -1,1 +1,15 @@
-# the crypt() function is a python module for Linux - can't do this yet
+import crypt # this is only available in the Linux version on python
+
+# testPass - takes encrypted password as parameter and returns
+# 1. after finding password
+# 2. after exhausting words in dictionary
+
+def testPass(cryptPass):
+    salt = cryptPass[0:2] # strips first two values of password
+    dictfile = open('dictionary.txt', 'r')
+    for word in dictfile.readlines():
+        word = word.strip('\n')
+        cryptWord = crypt.crypt(word, salt)
+        if (cryptWord == cryptPass):
+            print "[+] Found password: " + word + "\n"
+            return
